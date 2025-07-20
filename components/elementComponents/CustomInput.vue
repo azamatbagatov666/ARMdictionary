c127508d045fdfe19450dc4fb12880bd76b65754
azamatbagatov666 <saraerkan@hotmail.com>
2025-07-20 11:40:22 +0000

    The first public version

---
+++ components/elementComponents/CustomInput.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const model = defineModel()
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text'
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  autocomplete: {
+    type: String,
+    default: 'off'
+  },
+});
+
+
+</script>
+
+<template>
+  <input class="bg-white px-2 rounded-lg text-black border-2 border-[rgb(128,128,128)] h-8" v-model="model" 
+  :type="props.type" :autocomplete="props.autocomplete" :placeholder="props.placeholder">
+</template>
+
+<style scoped>
+/*a*/
+
+
+</style>
