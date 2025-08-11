482b16b512fcce5a77b0a65c1ec65386115f6e92
SARA ERKAN <saraerkan@hotmail.com>
2025-08-11 12:43:35 +0300

    package json modified for prod mode

--- pages/deneme.vue
+++ pages/deneme.vue
@@ -1,28 +1,13 @@
 <script setup lang="ts">
 
 
-import { useWindowSize } from '@vueuse/core';
-const { width } = useWindowSize();
-const deneme = ref();
 
-
-
-
-const getToday =  () => {
- console.log(width.value)
- console.log(width.value >= 1024)
- if (width.value <= 1024)
- deneme.value = true;
-
-};
 
 
 </script>
 
 <template>
 <div>
-    <PhosphorIconPhone :size="32" color="#fafafa" weight="fill" @click="getToday" />
-    <div v-if="deneme">denemelerce</div>
 </div>
 </template>
 
