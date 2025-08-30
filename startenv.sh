7fa09c725d9567b528792fa6e6602fe89febf9fc
SARA ERKAN <saraerkan@hotmail.com>
2025-08-31 00:11:56 +0300

    modified: start env modified

--- startenv.sh
+++ startenv.sh
@@ -1,9 +1,3 @@
-while IFS='=' read -r name value; do
-  # Skip empty lines and comments
-  [ -z "$name" ] && continue
-  [[ "$name" =~ ^# ]] && continue
-  
-  export "$name"="$value"
-done < .env
+export $(grep -v '^#' .env | xargs)
 
 nohup npm run start &
