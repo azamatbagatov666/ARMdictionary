6d96db831d62037eb105f0541aa443f630993cdb
azamatbagatov666 <saraerkan@hotmail.com>
2026-06-12 00:23:25 +0300

    Casing fix

---
+++ components/earchHistory.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { useSearchHistoryStore } from "~/store/search-history.store";
+
+const { t } = useI18n();
+
+
+const searchHistoryStore = useSearchHistoryStore();
+
+const emit = defineEmits<{
+  (e: "history-selected", data: string): void;
+}>();
+
+const buttonClick = (event: Event) => {
+  event.preventDefault();
+};
+
+const remove = () => {
+  searchHistoryStore.removeHistory();
+};
+
+const randomWordTimeout = ref(false);
+
+const historySelected = (item: string) => {
+  if (!randomWordTimeout.value) {
+    emit("history-selected", item);
+    randomWordTimeout.value = true;
+
+    setTimeout(() => {
+      randomWordTimeout.value = false;
+    }, 500);
+  }
+};
+
+const historyList = computed(() => searchHistoryStore.state.history);
+</script>
+
+<template>
+  <div
+    class="frame !border-0 lg:w-[600px] w-full"
+  >
+    <ul class="p-2 w-full">
+      <li
+        v-if="historyList.length > 0"
+        v-for="(item, index) in historyList"
+        @click="historySelected(item)"
+        class="inline-block mr-2"
+      >
+        <span
+          class="hover:underline hover:cursor-pointer  hover:bg-white dark:hover:bg-gray-500"
+          v-text="item"
+        ></span
+        ><span class="ml-2" v-if="index < historyList.length - 1" v-text="'/'"></span>
+      </li>
+      <span class="text-center w-full inline-block" v-else
+        >Arama geçmişinde hiç sözcük yok</span
+      >
+    </ul>
+    <button
+      v-if="historyList.length > 0"
+      @click="remove"
+      @mousedown="buttonClick"
+      class="bg-white border select-none border-black text-black font-bold px-4 h-8 rounded-md flex items-center mx-auto transition-colors duration-300 active:scale-95 active:bg-red-500"
+    >
+           <SVGTrash/>
+
+      
+      <span class="ml-2"
+        v-text="t('searchHistory.removeButton')"
+      ></span>
+    </button>
+
+    
+  </div>
+</template>
+
+<style scoped>
+
+@media (hover: hover) and (pointer: fine) {
+  .border:hover {
+@apply bg-red-500;
+
+  }
+
+
+
+}
+
+</style>
